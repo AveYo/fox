eb9930ba94e8949b3806c58bfb216bef82da7539
AveYo <aveyo@yahoo.com>
2021-02-25 04:23:20 +0200

    enable-UserChrome.js flr

---
+++ defaults/pref/enable-UserChrome.js
@@ -0,0 +1,3 @@
+/// create in Firefox-Install-Directory/defaults/pref/enable-UserChrome.js
+/// to enable advanced javascript access from Firefox-Install-Directory/UserChrome.js
+pref("general.config.filename", "UserChrome.js"); pref("general.config.obscure_value", 0); pref("general.config.sandbox_enabled", false); 
--- defaults/pref/enable-tweaks.js
+++
@@ -1,3 +0,0 @@
-// enable-tweaks.js - create in Firefox defaults pref directory - C:\Program Files\Mozilla Firefox\defaults\pref\enable-tweaks.js
-// enables advanced tweaks.js from Firefox executable directory - C:\Program Files\Mozilla Firefox\tweaks.js
-pref("general.config.filename", "tweaks.js"); pref("general.config.obscure_value", 0); pref("general.config.sandbox_enabled", false);
