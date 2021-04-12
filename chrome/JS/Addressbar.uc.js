ee309ac5964acbd279eb0672d04f2ad00e68360d
AveYo <aveyo@yahoo.com>
2021-04-13 02:29:47 +0300

    Addressbar redux
    Open input as URL on Enter - press Tab to Search instead

---
+++ chrome/JS/Addressbar.uc.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name            Addressbar redux
+// @author          AveYo
+// @description     Open input as URL on Enter - press Tab to Search instead
+// @include         main
+// @onlyonce
+// ==/UserScript==
+
+if (typeof UC === 'undefined') UC = {};
+
+UC.Addressbar = {
+  init: function() {
+    let UrlbarView = window.gURLBar.view;
+    UrlbarView.onQueryResults_uc = UrlbarView.onQueryResults;
+    UrlbarView.onQueryResults = function (queryContext) {
+      this.onQueryResults_uc(queryContext);
+      if (this.selectedElementIndex == 0 && !this.input.searchMode && !this.oneOffSearchButtons.selectedButton) {
+        let result = this.getResultAtIndex(0);
+        if (result?.payload.suggestion || result?.payload.query) {this.clearSelection();}
+      }
+    }; 
+    UrlbarView.input.handleCommand = function (event = null) {
+      let element = this.view.selectedElement;
+      let result = this.view.getResultFromElement(element);
+      let btn = this.view.oneOffSearchButtons.selectedButton;
+      if (result && result?.payload.keyword) {
+        this.pickResult(result, event);
+      }
+      else if (btn && (event instanceof this.window.MouseEvent === false || event.target == btn)) {
+        this.view.oneOffSearchButtons.handleSearchCommand(
+          event, {engineName: btn.engine?.name, source: btn.source, entry: "oneoff"}
+        );
+      }
+      else if (this.searchMode || this.view.selectedElementIndex == 0){
+        this.handleNavigation({event});
+      }
+      else if (this.value) {
+        let flags = Ci.nsIURIFixup.FIXUP_FLAG_FIX_SCHEME_TYPOS;
+        if (this.isPrivate) {flags |= Ci.nsIURIFixup.FIXUP_FLAG_PRIVATE_CONTEXT;}
+        let {preferredURI: uri, postData} = Services.uriFixup.getFixupURIInfo(this.value, flags);
+        this._loadURL(uri.spec, event, this._whereToOpen(event), {});
+      }
+    };
+    console.info('\u2713 Addressbar');
+  }
+};
+
+UC.Addressbar.init();
