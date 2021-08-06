1ac28dcc4cdb8c9fe54408990389ef58d0d10057
AveYo <aveyo@yahoo.com>
2021-08-06 18:54:09 +0300

    Update UserChrome.js
    bundled Addressbar.uc.js annoyance fix
    now will also pass anything with white-spaces to default handler (search)

--- UserChrome.js
+++ UserChrome.js
@@ -11,58 +11,6 @@ try {
     if (!window._gBrowser || !skip.test(location.href)) return; window.gBrowser = window._gBrowser;
 /***********************************************    PLACE JS SNIPPETS BELOW THIS LINE!    ***********************************************/
 
-// ==UserScript==
-// @name            Addressbar redux v2
-// @author          AveYo
-// @description     Open input as URL on Enter - press Tab to Search instead
-// @include         main
-// @onlyonce
-// ==/UserScript==
-
-if (typeof UC === 'undefined') UC = {};
-
-UC.Addressbar = {
-  init: function() {
-    XPCOMUtils.defineLazyModuleGetters(this, {
-      UrlbarView: "resource:///modules/UrlbarView.jsm",
-      UrlbarInput: "resource:///modules/UrlbarInput.jsm",
-    });
-    this.UrlbarView.prototype.onQueryResults_uc = this.UrlbarView.prototype.onQueryResults;
-    this.UrlbarView.prototype.onQueryResults = function (queryContext) {
-      this.onQueryResults_uc(queryContext);
-      if (this.selectedElementIndex == 0 && !this.input.searchMode && !this.oneOffSearchButtons.selectedButton) {
-        let result = this.getResultAtIndex(0);
-        if (result?.payload.suggestion || result?.payload.query) {this.clearSelection();}
-      }
-    }; 
-    this.UrlbarInput.prototype.handleCommand = function (event = null) {
-      let element = this.view.selectedElement;
-      let result = this.view.getResultFromElement(element);
-      let btn = this.view.oneOffSearchButtons.selectedButton;
-
-      if (result && result?.payload.keyword) {
-        this.pickResult(result, event);
-      }
-      else if (btn && (event instanceof this.window.MouseEvent === false || event.target == btn)) {
-        this.view.oneOffSearchButtons.handleSearchCommand(
-          event, {engineName: btn.engine?.name, source: btn.source, entry: "oneoff"}
-        );
-      }
-      else if (this.searchMode || this.view.selectedElementIndex == 0){
-        this.handleNavigation({event});
-      }
-      else if (this.value) {
-        let flags = Ci.nsIURIFixup.FIXUP_FLAG_FIX_SCHEME_TYPOS;
-        if (this.isPrivate) {flags |= Ci.nsIURIFixup.FIXUP_FLAG_PRIVATE_CONTEXT;}
-        let {preferredURI: uri, postData} = Services.uriFixup.getFixupURIInfo(this.value, flags);
-        this._loadURL(uri.spec, event, this._whereToOpen(event), {});
-      }
-    };
-    console.info('\u2713 Addressbar');
-  }
-};
-
-UC.Addressbar.init();
 
 // ==UserScript==
 // @name            OneClickSearch redux v3
@@ -101,6 +49,59 @@ UC.OneClickSearch = {
 UC.OneClickSearch.init();
 
 // ==UserScript==
+// @name            Addressbar redux v3
+// @author          AveYo
+// @description     Open input as URL on Enter - press Tab to Search instead
+// @include         main
+// @onlyonce
+// ==/UserScript==
+
+if (typeof UC === 'undefined') UC = {};
+
+UC.Addressbar = {
+  init: function() {
+    XPCOMUtils.defineLazyModuleGetters(this, {
+      UrlbarView: "resource:///modules/UrlbarView.jsm",
+      UrlbarInput: "resource:///modules/UrlbarInput.jsm",
+    });
+    this.UrlbarView.prototype.onQueryResults_uc = this.UrlbarView.prototype.onQueryResults;
+    this.UrlbarView.prototype.onQueryResults = function (queryContext) {
+      this.onQueryResults_uc(queryContext);
+      if (this.selectedElementIndex == 0 && !this.input.searchMode && !this.oneOffSearchButtons.selectedButton) {
+        let result = this.getResultAtIndex(0);
+        if (result?.payload.suggestion || result?.payload.query) {this.clearSelection();}
+      }
+    }; 
+    this.UrlbarInput.prototype.handleCommand = function (event = null) {
+      let element = this.view.selectedElement;
+      let result = this.view.getResultFromElement(element);
+      let btn = this.view.oneOffSearchButtons.selectedButton;
+
+      if (result && result?.payload.keyword) {
+        this.pickResult(result, event);
+      }
+      else if (btn && (event instanceof this.window.MouseEvent === false || event.target == btn)) {
+        this.view.oneOffSearchButtons.handleSearchCommand(
+          event, {engineName: btn.engine?.name, source: btn.source, entry: "oneoff"}
+        );
+      }
+      else if (this.searchMode || this.view.selectedElementIndex == 0 || (this.value && /\s/.test(this.value)) {
+        this.handleNavigation({event});
+      }
+      else if (this.value && /\s/.test(this.value) === false) {
+        let flags = Ci.nsIURIFixup.FIXUP_FLAG_FIX_SCHEME_TYPOS;
+        if (this.isPrivate) {flags |= Ci.nsIURIFixup.FIXUP_FLAG_PRIVATE_CONTEXT;}
+        let {preferredURI: uri, postData} = Services.uriFixup.getFixupURIInfo(this.value, flags);
+        this._loadURL(uri.spec, event, this._whereToOpen(event), {});
+      }
+    };
+    console.info('\u2713 Addressbar');
+  }
+};
+
+UC.Addressbar.init();
+
+// ==UserScript==
 // @name            SimpleHotkeysOverride v2
 // @author          AveYo
 // @description     see Firefox-Install-Directory/browser/omni.ja/chrome/browser/content/browser/browser.xhtml - 'mainKeyset'
@@ -130,7 +131,7 @@ UC.SimpleHotkeysOverride = {
   }
 };
 
-UC.SimpleHotkeysOverride.init();
+//UC.SimpleHotkeysOverride.init();
 
 /***********************************************    PLACE JS SNIPPETS ABOVE THIS LINE!    ***********************************************/
 } }; if (!Services.appinfo.inSafeMode) new UserChromeJS(); } catch(fox) {};
