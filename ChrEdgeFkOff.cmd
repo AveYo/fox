2d39fc43333b06e7bbe504519d8437aff4dfe800
AveYo <aveyo@yahoo.com>
2021-11-15 03:30:07 +0200

    v1.2 make start menu web search or widgets links open in your chosen default browser
    switched from using system32 path for the script, to programdata (might help with some lame av / configuration)

--- ChrEdgeFkOff.cmd
+++ ChrEdgeFkOff.cmd
@@ -12,12 +12,12 @@ Set W = CreateObject("WScript.Shell"): Set E = W.Environment( "Process" ): E("CL
 C = "[diagnostics.process]::start([uri]::unescapedatastring(($env:CL-split'&url[=]',2)[1]).Trim([char]34))"
 W.Run "powershell -win 1 -nop -c " & C, 0, False
 ''>''; to undo, "delete HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe" via regedit #> 
-'@; $file = join-path $([environment]::SystemDirectory) "ChrEdgeFkOff.vbs"; $cmd = "wscript $file //B //T:5"
+'@; $file = join-path ([Environment]::GetFolderPath('CommonApplicationData')) "ChrEdgeFkOff.vbs"; $cmd = "wscript $file //B //T:5"
 if (test-path $file) {
   remove-itemproperty $key 'Debugger' -force -ea 0 >''; del $file -force -ea 0 
-  write-host -fore 0xf -back 0xd "`n ChrEdgeFkOff v1.1 [REMOVED] run again to install " } else {
+  write-host -fore 0xf -back 0xd "`n ChrEdgeFkOff v1.2 [REMOVED] run again to install " } else {
   new-item $key -force -ea 0 >''; set-itemproperty $key 'Debugger' $cmd -force -ea 0; [io.file]::WriteAllText($file, $vbs)
-  write-host -fore 0xf -back 0x2 "`n ChrEdgeFkOff v1.1 [INSTALLED] run again to remove " } ; timeout /t 5
+  write-host -fore 0xf -back 0x2 "`n ChrEdgeFkOff v1.2 [INSTALLED] run again to remove " } ; timeout /t 5
 } ; start -verb runas powershell -args "-nop -c & {`n`n$($_Paste_in_Powershell-replace'"','\"')}"
 $_Press_Enter
 #::
