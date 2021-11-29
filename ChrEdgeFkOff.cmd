243ebf377efd743decc7f4901d9ffcb9fde92972
AveYo <aveyo@yahoo.com>
2021-11-29 21:59:54 +0200

    v1.3
    standard edge icon opens chosen default browser instead of nothing - feels great to click edge and have firefox open ;)
    if somehow the default browser is set back to standard edge, script will prevent IFEO loop by prompting user to delete the redirect
    adapted the windows 11 notepad.exe way of IFEO handling -  this opens up the possibility to still use Edge Beta / Dev / Canary (or Standard with different path)
    for more granularity try out rcmaehl's MSEdgeRedirect project

--- ChrEdgeFkOff.cmd
+++ ChrEdgeFkOff.cmd
@@ -2,22 +2,32 @@
 #:: double-click to run or just copy-paste into powershell - it's a standalone hybrid script
 #::
 #:: ChrEdgeFkOff - make start menu web search or widgets links open in your chosen default browser - by AveYo
-#:: this will obviously block msedge.exe while installed, but you can easily revert it with this toggle script when needed
+#:: this will obviously block standard msedge.exe while installed, but you can use Beta / Dev / Canary instead
+#:: v1.3 standard edge icon opens chosen default browser; prevent IFEO loop - thanks rcmaehl for the heads-up   
 #:: 
 $_Paste_in_Powershell = {
 $key = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe'; $vbs = @'
-''>''; <# ChrEdgeFkOff - make start menu web search or widgets links open in your chosen default browser - by AveYo  
+' ChrEdgeFkOff - make start menu web search or widgets links open in your chosen default browser - by AveYo  
 Dim C, A: For Each i in WScript.Arguments: A = A&" """&i&"""": Next
 Set W = CreateObject("WScript.Shell"): Set E = W.Environment( "Process" ): E("CL") = A
-C = "[diagnostics.process]::start([uri]::unescapedatastring(($env:CL-split'&url[=]',2)[1]).Trim([char]34))"
-W.Run "powershell -win 1 -nop -c " & C, 0, False
-''>''; to undo, "delete HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe" via regedit #> 
+C = "$U=get-itemproperty 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice' 'ProgID';"
+C = C & "$C=get-itemproperty -lit ""Registry::HKCR\$($U.ProgID)\shell\open\command"" '(Default)' -ea 0;"
+C = C & "$UserChoice=($C.'(Default)'-split [char]34,3)[1];"
+C = C & "$undo='-c del ''HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe\0'' -force';"
+C = C & "if ($UserChoice -like '*Microsoft\Edge\Application\msedge.exe*') {start powershell $undo -verb runas} "
+C = C & "else {start $UserChoice $([uri]::unescapedatastring(($env:CL-split'&url[=]',2)[1]).Trim([char]34)+' ')}"
+W.Run "powershell -nop -c " & C, 0, False
 '@; $file = join-path ([Environment]::GetFolderPath('CommonApplicationData')) "ChrEdgeFkOff.vbs"; $cmd = "wscript $file //B //T:5"
-if (test-path $file) {
-  remove-itemproperty $key 'Debugger' -force -ea 0 >''; del $file -force -ea 0 
-  write-host -fore 0xf -back 0xd "`n ChrEdgeFkOff v1.2 [REMOVED] run again to install " } else {
-  new-item $key -force -ea 0 >''; set-itemproperty $key 'Debugger' $cmd -force -ea 0; [io.file]::WriteAllText($file, $vbs)
-  write-host -fore 0xf -back 0x2 "`n ChrEdgeFkOff v1.2 [INSTALLED] run again to remove " } ; timeout /t 5
+if (test-path "$key\0") {
+  remove-item "$key\0" -rec -force -ea 0 >''; remove-itemproperty $key 'Debugger' -force -ea 0 >''; del $file -force -ea 0 >''
+  write-host -fore 0xf -back 0xd "`n ChrEdgeFkOff v1.3 [REMOVED] run again to install "
+} else {                              
+  new-item "$key\0" -force -ea 0 >''; remove-itemproperty $key 'Debugger' -force -ea 0 >''; [io.file]::WriteAllText($file, $vbs)
+  $P = 'ProgramFiles'; if ([Environment]::Is64BitOperatingSystem) {$P+='x86'}
+  $E = join-path ([Environment]::GetFolderPath($P)) 'Microsoft\Edge\Application\msedge.exe'
+  set-itemproperty "$key\0" 'FilterFullPath' $E -force -ea 0; set-itemproperty $key 'UseFilter' 1 -type dword -force -ea 0
+  set-itemproperty "$key\0" 'Debugger' $cmd -force -ea 0 
+  write-host -fore 0xf -back 0x2 "`n ChrEdgeFkOff v1.3 [INSTALLED] run again to remove " } ; timeout /t 5
 } ; start -verb runas powershell -args "-nop -c & {`n`n$($_Paste_in_Powershell-replace'"','\"')}"
 $_Press_Enter
 #::
