c4bcabfa94232e1e01f3f63b95daac915dcdd2a7
AveYo <aveyo@yahoo.com>
2022-02-08 07:36:30 +0200

    v2.1
    fix help urls

--- ChrEdgeFkOff.cmd
+++ ChrEdgeFkOff.cmd
@@ -2,7 +2,7 @@
 #:: double-click to run or just copy-paste into powershell - it's a standalone hybrid script
 #::
 #:: ChrEdgeFkOff - make start menu web search or widgets links open in your chosen default browser - by AveYo
-#:: v2.0 only redirects microsoft-edge: links, no longer blocks msedge.exe (with a junction trick)    
+#:: v2.0 only redirects microsoft-edge: links, no longer blocks msedge.exe (with a junction trick); v2.1 fix help urls    
 #:: 
 $_Paste_in_Powershell = {
 $vbs = @'
@@ -10,11 +10,11 @@ $vbs = @'
 Dim C, A: For Each i in WScript.Arguments: A = A&" """&i&"""": Next
 Set W = CreateObject("WScript.Shell"): Set E = W.Environment( "Process" ): E("CL") = A : C = ""
 C = C & "$U = get-itemproperty 'HKCU:\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice' 'ProgID';"
-C = C & "$C = get-itemproperty -lit $('Registry::HKCR\' + $U.ProgID + '\shell\open\command') '(Default)' -ea 0;"
+C = C & "$C = get-itemproperty -lit $('Registry::HKCR\' + $U.ProgID + '\shell\open\command') '(Default)' -ea 0; $q=[char]34;"
 C = C & "$UserChoice = ($C.'(Default)'-split [char]34,3)[1]; $MSE = $env:CL -replace '\\Microsoft\\Edge', '\Microsoft\ChrEdge';"
 C = C & "if ($UserChoice -like '*Microsoft\Edge\Application\msedge.exe*') {iex('&'+$MSE); return 2};"
 C = C & "if ($env:CL -notlike '*microsoft-edge:*') {iex('&'+$MSE); return 1};"
-C = C & "start $UserChoice $([uri]::unescapedatastring(($env:CL-split'(?=http[s]?)')[1] -replace [char]34)+' '); return 0"
+C = C & "start $UserChoice $($q+ ([uri]::unescapedatastring(($env:CL -split'(?=http[s]?)',2)[1]) -replace $q) +$q); return 0"
 W.Run "powershell -nop -c " & C, 0, False
 '@ 
 $DATA = [Environment]::GetFolderPath('CommonApplicationData'); $file = join-path $DATA "ChrEdgeFkOff.vbs"
@@ -24,14 +24,14 @@ $IFEO = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution
 if (test-path "$IFEO\0") {
   remove-item "$IFEO\0" -rec -force -ea 0 >''; remove-itemproperty $IFEO 'Debugger' -force -ea 0 >'' 
   del $file -force -ea 0 >''; rmdir $CREDGE -rec -force -ea 0 >'';
-  write-host -fore 0xf -back 0xd "`n ChrEdgeFkOff v2.0 [REMOVED] run again to install "
+  write-host -fore 0xf -back 0xd "`n ChrEdgeFkOff v2.1 [REMOVED] run again to install "
 } else {                              
   new-item "$IFEO\0" -force -ea 0 >''; remove-itemproperty $IFEO 'Debugger' -force -ea 0 >'' 
   [io.file]::WriteAllText($file, $vbs) >''; start -nonew cmd "/d/x/r mklink /J ""$CREDGE"" ""$EDGE"" >nul"
   set-itemproperty $IFEO 'UseFilter' 1 -type dword -force -ea 0
   set-itemproperty "$IFEO\0" 'FilterFullPath' $(join-path $PROF 'Microsoft\Edge\Application\msedge.exe') -force -ea 0 
   set-itemproperty "$IFEO\0" 'Debugger' "wscript $file //B //T:5" -force -ea 0 
-  write-host -fore 0xf -back 0x2 "`n ChrEdgeFkOff v2.0 [INSTALLED] run again to remove " } ; timeout /t 5
+  write-host -fore 0xf -back 0x2 "`n ChrEdgeFkOff v2.1 [INSTALLED] run again to remove " } ; timeout /t 5
 } ; start -verb runas powershell -args "-nop -c & {`n`n$($_Paste_in_Powershell-replace'"','\"')}"
 $_Press_Enter
 #::
