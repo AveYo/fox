19afb3bf08fbc87d1fff3a158fdf92bf56ddb0e7
AveYo <aveyo@yahoo.com>
2022-07-17 16:37:52 +0000

    2022.07.17
    use cmd /c for reg commands to prevent quotes parsing / localization issues
    fix slavic localization issue with reg query (thanks westyles for reporting it)

--- Edge_Removal.bat
+++ Edge_Removal.bat
@@ -1,27 +1,27 @@
 @(set "0=%~f0"^)#) & powershell -nop -c iex([io.file]::ReadAllText($env:0)) & exit/b
 #:: double-click to run or just copy-paste into powershell - it's a standalone hybrid script
 #::
-$_Paste_in_Powershell = { $host.ui.RawUI.WindowTitle = 'Edge Removal - AveYo, 2022.06.21'
+$_Paste_in_Powershell = { $host.ui.RawUI.WindowTitle = 'Edge Removal - AveYo, 2022.07.17'
 
-$also_remove_webview = 1 
+$also_remove_webview = 1
 
 ## targets
 $remove_win32 = @("Microsoft Edge","Microsoft Edge Update"); $remove_appx = @("MicrosoftEdge")
 if ($also_remove_webview -eq 1) {$remove_win32 += "Microsoft EdgeWebView"; $remove_appx += "Win32WebViewHost"}
-## enable admin privileges 
+## enable admin privileges
 $D1=[uri].module.gettype('System.Diagnostics.Process')."GetM`ethods"(42) |where {$_.Name -eq 'SetPrivilege'} #`:no-ev-warn
 'SeSecurityPrivilege','SeTakeOwnershipPrivilege','SeBackupPrivilege','SeRestorePrivilege'|foreach {$D1.Invoke($null, @("$_",2))}
 ## set useless policies
 foreach ($p in 'HKLM\SOFTWARE\Policies','HKLM\SOFTWARE') {
-  reg add "$p\Microsoft\EdgeUpdate" /f /v InstallDefault /d 0 /t reg_dword >''
-  reg add "$p\Microsoft\EdgeUpdate" /f /v "Install{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062}" /d 0 /t reg_dword >''
-  reg add "$p\Microsoft\EdgeUpdate" /f /v "Install{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}" /d 1 /t reg_dword >''
-  reg add "$p\Microsoft\EdgeUpdate" /f /v DoNotUpdateToEdgeWithChromium /d 1 /t reg_dword >''
+  cmd /c "reg add ""$p\Microsoft\EdgeUpdate"" /f /v InstallDefault /d 0 /t reg_dword >nul 2>nul"
+  cmd /c "reg add ""$p\Microsoft\EdgeUpdate"" /f /v Install{56EB18F8-B008-4CBD-B6D2-8C97FE7E9062} /d 0 /t reg_dword >nul 2>nul"
+  cmd /c "reg add ""$p\Microsoft\EdgeUpdate"" /f /v Install{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5} /d 1 /t reg_dword >nul 2>nul"
+  cmd /c "reg add ""$p\Microsoft\EdgeUpdate"" /f /v DoNotUpdateToEdgeWithChromium /d 1 /t reg_dword >nul 2>nul"
 }
 ## clear win32 uninstall block
 foreach ($hk in 'HKCU','HKLM') {foreach ($wow in '','\Wow6432Node') {foreach ($i in $remove_win32) {
-  reg delete "$hk\SOFTWARE${wow}\Microsoft\Windows\CurrentVersion\Uninstall\$i" /f /v NoRemove 2>&1 >''
-}}}  
+  cmd /c "reg delete ""$hk\SOFTWARE${wow}\Microsoft\Windows\CurrentVersion\Uninstall\$i"" /f /v NoRemove >nul 2>nul"
+}}}
 ## find all Edge setup.exe
 $setup = @(); $bho = @(); "LocalApplicationData","ProgramFilesX86","ProgramFiles" |foreach {
   $setup += dir $($([Environment]::GetFolderPath($_)) + '\Microsoft\Edge*\setup.exe') -rec -ea 0
@@ -38,19 +38,19 @@ $users = @('S-1-5-18'); if (test-path $store) {$users += $((dir $store |where {$
 foreach ($choice in $remove_appx) { if ('' -eq $choice.Trim()) {continue}
   foreach ($appx in $($provisioned |where {$_.PackageName -like "*$choice*"})) {
     $PackageFamilyName = ($appxpackage |where {$_.Name -eq $appx.DisplayName}).PackageFamilyName; $PackageFamilyName
-    reg add "$store_reg\Deprovisioned\$PackageFamilyName" /f 2>&1 >'' 
-    dism /online /remove-provisionedappxpackage /packagename:$($appx.PackageName) 2>&1 >''
-    #powershell -nop -c remove-appxprovisionedpackage -packagename "'$($appx.PackageName)'" -online 2>&1 >'' 
+    cmd /c "reg add ""$store_reg\Deprovisioned\$PackageFamilyName"" /f >nul 2>nul"
+    cmd /c "dism /online /remove-provisionedappxpackage /packagename:$($appx.PackageName) >nul 2>nul"
+    #powershell -nop -c remove-appxprovisionedpackage -packagename "'$($appx.PackageName)'" -online 2>&1 >''
   }
   foreach ($appx in $($appxpackage |where {$_.PackageFullName -like "*$choice*"})) {
     $inbox = (gp "$store\InboxApplications\*$($appx.Name)*" Path).PSChildName
     $PackageFamilyName = $appx.PackageFamilyName; $PackageFullName = $appx.PackageFullName; $PackageFullName
-    foreach ($app in $inbox) {reg delete "$store_reg\InboxApplications\$app" /f 2>&1 >'' }
-    reg add "$store_reg\Deprovisioned\$PackageFamilyName" /f 2>&1 >''
-    foreach ($sid in $users) {reg add "$store_reg\EndOfLife\$sid\$PackageFullName" /f 2>&1 >''}
-    dism /online /set-nonremovableapppolicy /packagefamily:$PackageFamilyName /nonremovable:0 2>&1 >''
-    powershell -nop -c remove-appxpackage -package "'$PackageFullName'" -AllUsers 2>&1 >''
-    foreach ($sid in $users) {reg delete "$store_reg\EndOfLife\$sid\$PackageFullName" /f 2>&1 >''}
+    foreach ($app in $inbox) {cmd /c "reg delete ""$store_reg\InboxApplications\$app"" /f >nul 2>nul" }
+    cmd /c "reg add ""$store_reg\Deprovisioned\$PackageFamilyName"" /f >nul 2>nul"
+    foreach ($sid in $users) {cmd /c "reg add ""$store_reg\EndOfLife\$sid\$PackageFullName"" /f >nul 2>nul"}
+    cmd /c "dism /online /set-nonremovableapppolicy /packagefamily:$PackageFamilyName /nonremovable:0 >nul 2>nul"
+    powershell -nop -c "remove-appxpackage -package '$PackageFullName' -AllUsers" 2>&1 >''
+    foreach ($sid in $users) {cmd /c "reg delete ""$store_reg\EndOfLife\$sid\$PackageFullName"" /f >nul 2>nul"}
   }
 }
 ## shut edge down, again
@@ -61,29 +61,31 @@ if ($also_remove_webview -eq 1) { foreach ($s in $setup) { try{ start -wait $s -
 foreach ($s in $setup) { try{ start -wait $s -args "--msedge $purge" } catch{} }
 ## prevent latest cumulative update (LCU) failing due to non-matching EndOfLife Edge entries
 foreach ($i in $remove_appx) {
-  dir "$store\EndOfLife" -rec -ea 0 |where {$_ -like "*${i}*"} |foreach {reg delete "$($_.Name)" /f 2>&1 >''}
-  dir "$store\Deleted\EndOfLife" -rec -ea 0 |where {$_ -like "*${i}*"} |foreach {reg delete "$($_.Name)" /f 2>&1 >''}
+  dir "$store\EndOfLife" -rec -ea 0 |where {$_ -like "*${i}*"} |foreach {cmd /c "reg delete ""$($_.Name)"" /f >nul 2>nul"}
+  dir "$store\Deleted\EndOfLife" -rec -ea 0 |where {$_ -like "*${i}*"} |foreach {cmd /c "reg delete ""$($_.Name)"" /f >nul 2>nul"}
 }
 
-## add ChrEdgeFkOff to redirect microsoft-edge: anti-competitive links to the default browser 
+## add ChrEdgeFkOff to redirect microsoft-edge: anti-competitive links to the default browser
 ##################################################################################################################################
 $ChrEdgeFkOff = @'
 @echo off
 ::# toggle when launched without arguments, else jump to arguments: "install" or "remove"
-set CLI=%*& set IFEO=HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options& set MSE=& set BHO=&;
-for /f "tokens=2*" %%V in ('reg query "HKCR\MSEdgeMHT\shell\open\command" /ve 2^>nul') do set "ProgID=%%W"
-for %%W in (%ProgID%) do if not defined MSE set "MSE=%%~W"& set "MSEPath=%%~dpW"
+set CLI=%*&(set IFEO=HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options&set MSE=&set BHO=&set ProgID=)
+for /f "tokens=* delims=" %%. in ('reg query "HKCR\MSEdgeMHT\shell\open\command" /ve /z 2^>nul') do set "ProgID=%%."
+if defined ProgID set "ProgID=%ProgID:*)    =%"
+if defined ProgID set "ProgID=%ProgID:*)    =%"
+for %%. in (%ProgID%) do if not defined MSE set "MSE=%%~."& set "MSEPath=%%~dp."
 if /i "%CLI%"=="" reg query "%IFEO%\ie_to_edge_stub.exe\0" /v Debugger >nul 2>nul && goto remove || goto install
 if /i "%~1"=="install" (goto install) else if /i "%~1"=="remove" goto remove
 
 :install
-if defined MSEPath for /f "delims=" %%W in ('dir /o:D /b /s "%MSEPath%\*ie_to_edge_stub.exe" 2^>nul') do set "BHO=%%~fW" 
+if defined MSEPath for /f "delims=" %%W in ('dir /o:D /b /s "%MSEPath%\*ie_to_edge_stub.exe" 2^>nul') do set "BHO=%%~fW"
 if not exist "%MSEPath%chredge.exe" if exist "%MSE%" mklink /h "%MSEPath%chredge.exe" "%MSE%" >nul
 if defined BHO copy /y "%BHO%" "%ProgramData%\\" >nul 2>nul
 call :export ChrEdgeFkOff.vbs > "%ProgramData%\ChrEdgeFkOff.vbs"
 reg add HKCR\microsoft-edge /f /ve /d URL:microsoft-edge >nul
 reg add HKCR\microsoft-edge /f /v "URL Protocol" /d "" >nul
-reg add HKCR\microsoft-edge /f /v "NoOpenWith" /d "" >nul 
+reg add HKCR\microsoft-edge /f /v "NoOpenWith" /d "" >nul
 reg add HKCR\microsoft-edge\shell\open\command /f /ve /d "\\"%ProgramData%\ie_to_edge_stub.exe\\" %%1" >nul
 reg add HKCR\MSEdgeHTM /f /v "NoOpenWith" /d "" >nul
 reg add HKCR\MSEdgeHTM\shell\open\command /f /ve /d "\\"%ProgramData%\ie_to_edge_stub.exe\\" %%1" >nul
@@ -93,10 +95,10 @@ reg add "%IFEO%\ie_to_edge_stub.exe\0" /f /v Debugger /d "wscript.exe \\"%Progra
 reg add "%IFEO%\msedge.exe" /f /v UseFilter /d 1 /t reg_dword >nul
 reg add "%IFEO%\msedge.exe\0" /f /v FilterFullPath /d "%MSE%" >nul
 reg add "%IFEO%\msedge.exe\0" /f /v Debugger /d "wscript.exe \\"%ProgramData%\ChrEdgeFkOff.vbs\\" //B //T:60" >nul
-exit /b 
+exit /b
 
 :remove
-del /f /q "%ProgramData%\ChrEdgeFkOff.vbs" "%MSEPath%chredge.exe" >nul 2>nul 
+del /f /q "%ProgramData%\ChrEdgeFkOff.vbs" "%MSEPath%chredge.exe" >nul 2>nul
 rem del /f /q "%ProgramData%\ie_to_edge_stub.exe"
 reg delete HKCR\microsoft-edge /f /v "NoOpenWith" >nul 2>nul
 reg add HKCR\microsoft-edge\shell\open\command /f /ve /d "\\"%MSE%\\" --single-argument %%1" >nul
@@ -108,13 +110,13 @@ exit /b
 
 :export: [USAGE] call :export NAME
 setlocal enabledelayedexpansion || Prints all text between lines starting with :NAME:[ and :NAME:] - A pure batch snippet by AveYo
-set [=&for /f "delims=:" %%s in ('findstr /nbrc:":%~1:\[" /c:":%~1:\]" "%~f0"')do if defined [ (set /a ]=%%s-3)else set /a [=%%s-1 
+set [=&for /f "delims=:" %%s in ('findstr /nbrc:":%~1:\[" /c:":%~1:\]" "%~f0"')do if defined [ (set /a ]=%%s-3)else set /a [=%%s-1
 <"%~f0" ((for /l %%i in (0 1 %[%) do set /p =)&for /l %%i in (%[% 1 %]%) do (set txt=&set /p txt=&echo(!txt!)) &endlocal &exit /b
 
 :ChrEdgeFkOff_vbs:[
-' ChrEdgeFkOff v4 - make start menu web search, widgets links or help open in your chosen default browser - by AveYo  
+' ChrEdgeFkOff v4 - make start menu web search, widgets links or help open in your chosen default browser - by AveYo
 Dim A,F,CLI,URL,decode,utf8,char,u,u1,u2,u3,ProgID,Choice : CLI = "": URL = "": For i = 1 to WScript.Arguments.Count - 1
-A = WScript.Arguments(i): CLI = CLI & " " & A: If InStr(1, A, "microsoft-edge:", 1) Then: URL = A: End If: Next 
+A = WScript.Arguments(i): CLI = CLI & " " & A: If InStr(1, A, "microsoft-edge:", 1) Then: URL = A: End If: Next
 
 decode = Split(URL,"%"): u = 0: Do While u <= UBound(decode): If u <> 0 Then
 char = Left(decode(u),2): If "&H" & Left(char,2) >= 128 Then
@@ -148,7 +150,7 @@ write-host -fore green @'
  $ffsetup='https://download.mozilla.org/?product=firefox-latest&os=win';
  $firefox="$([Environment]::GetFolderPath('Desktop'))\FirefoxSetup.exe";
  (new-object System.Net.WebClient).DownloadFile($ffsetup,$firefox); start $firefox
-'@;''  
+'@;''
 
 } ; start -verb runas powershell -args "-nop -noe -c & {`n`n$($_Paste_in_Powershell-replace'"','\"')}"
 $_Press_Enter
